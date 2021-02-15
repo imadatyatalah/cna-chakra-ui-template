31fdf97b5ba34a366a6126fc0e7af86333b7e2f5
imad <imadatyatalah@gmail.com>
2021-02-15 14:56:41 +0100

    add font weight 600

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,15 +4,14 @@ import Layout from "../components/layout";
 
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import "@fontsource/poppins/800.css";
 
 const theme = extendTheme({
   fonts: {
-    body:
-      "Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-    heading:
-      "Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+    body: "Poppins, -apple-system",
+    heading: "Poppins, -apple-system",
   },
   colors: {
     customYellow: "#FFCF3C",
