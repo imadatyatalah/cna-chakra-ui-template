db39b5acd2ff9b93d0d4c205442dcc66a4957b10
imad <imadatyatalah@gmail.com>
2021-02-22 08:42:32 +0100

    fix background color

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,25 +3,23 @@ import { Box } from "@chakra-ui/react";
 import Logo from "../UI/logo";
 import Navbar from "./navbar";
 
-const Header = () => {
-  return (
-    <>
-      <Box as="header" bg="customYellow" color="black">
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          px={[4, 6, 10, 14, 20]}
-          maxW="1440px"
-          mx="auto"
-          h="4rem"
-        >
-          <Logo />
-          <Navbar />
-        </Box>
+const Header = () => (
+  <>
+    <Box as="header" bg="telegram.500" color="white">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        px={[4, 6, 10, 14, 20]}
+        maxW="1440px"
+        mx="auto"
+        h="4rem"
+      >
+        <Logo />
+        <Navbar />
       </Box>
-    </>
-  );
-};
+    </Box>
+  </>
+);
 
 export default Header;
